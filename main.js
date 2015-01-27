bc3c12f25e06fc17426a4c6fd5775a64833234a5
Michael Germany <mike@rickgermany.com>
2015-01-26 17:12:08 -0700

    final commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).on('ready', function() {
+  $(document).on("click", function(event) {
+
+  	//store the coordinates in a variable
+  	var x = event.pageX;
+  	var y = event.pageY;
+
+  	var marker = $("<div class='mapclass'>you are here</div>");
+  	marker.css({position: "absolute", top: y, left: x});
+
+  	$(".container").append(marker);
+  	$(document).on("click", function() {
+  		$(marker).remove();
+  	});
+	});
+});
